ecffeaff39ca483220f9dc97150fa255e79a6c4b
Rajesh Pandit <rashq122@gmail.com>
2026-07-17 10:09:06 +0530

    Time: 0 ms (100%), Space: 14.3 MB (11.92%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string s, int k) {
+    int solve(string s,int k,char ch){
         int n = s.size();
         int i = 0;
         int j = 0;
         int ans = -1;
         int wr = 0;
+        char rv = ch=='T'?'F':'T';
+
         while(j<n){
             while(j<n && wr<k){
-                if(s[j] == 'F')wr++;
+                if(s[j] == ch)wr++;
                 j++;
             }
-            while(j < n && s[j] == 'T'){
-                j++;
 
-            }
-            ans = max(j-i,ans);
-
-            if(s[i]=='F')wr--;
-
-            i++;
-        }
-
-        i = 0;
-        j = 0;
-        wr = 0;
-        while(j<n){
-            while(j<n && wr<k){
-                if(s[j] == 'T')wr++;
+            while(j<n && s[j]==rv){
                 j++;
             }
-            while(j < n && s[j] == 'F'){
-                j++;
-
-            }
-            ans = max(j-i,ans);
-
-            if(s[i]=='T')wr--;
+            ans=max(j-i,ans);
+            if(s[i]==ch)wr--;
 
             i++;
         }
 
         return ans;
+    }
+    int maxConsecutiveAnswers(string s, int k) {
+        return max(solve(s,k,'T'),solve(s,k,'F'));
     }
 };
