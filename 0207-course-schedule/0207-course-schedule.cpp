d4d956cf032e1ed9db64c7b762be9a168c1541ef
Rajesh Pandit <rashq122@gmail.com>
2026-07-20 23:17:36 +0530

    Time: 11 ms (20.52%), Space: 20.1 MB (24.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool dfs(int u,unordered_map<int,vector<int>>& adj,vector<bool>& visited,vector<bool>& pathVisited){
-        visited[u] = true;
-        pathVisited[u] = true;
-
-        for(auto& v : adj[u]){
-            if(pathVisited[v])return true;
-            if(!visited[v] && dfs(v,adj,visited,pathVisited))return true;
-        }
-        pathVisited[u] = false;
-
-        return false;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V = numCourses;
+    bool canFinish(int V, vector<vector<int>>& edges) {
         unordered_map<int,vector<int>> adj;
-        for(auto& ed : prerequisites){
-            adj[ed[0]].push_back(ed[1]);
+        vector<int> indegree(V,0);
+
+        for(auto& ed : edges){
+            adj[ed[1]].push_back(ed[0]);
+            indegree[ed[0]]++;
         }
 
-        vector<bool> pathVisited(V,false);
-        vector<bool> visited(V,false);
-
+        queue<int> q;
         for(int i=0;i<V;i++){
-            if(!visited[i] && dfs(i,adj,visited,pathVisited))return false;
+            if(indegree[i]==0)q.push(i);
         }
 
-        return true;
+        int count = 0;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            count++;
+
+            for(auto& v : adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0)q.push(v);
+            }
+        }
+
+        return V==count;
+
     }
 };
