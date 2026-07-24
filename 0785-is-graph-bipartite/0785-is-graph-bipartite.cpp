c1b8913f52154acc0dc92e5a117e14c8e2f94346
Rajesh Pandit <rashq122@gmail.com>
2026-07-24 22:28:19 +0530

    Time: 7 ms (5.07%), Space: 20.9 MB (5.18%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-    bool dfs(int u,unordered_map<int,vector<int>>& adj,vector<int>& color, int currColor){
+    bool bfs(int u,unordered_map<int,vector<int>>& adj,vector<int>& color,int currColor){
         color[u] = currColor;
-
-        for(auto& v : adj[u]){
-            if(color[v] == currColor)return false;
-            if(color[v]==-1 && !dfs(v,adj,color,1-currColor))return false;
+        queue<int> q;
+        q.push(u);
+        
+        while(!q.empty()){
+            int U = q.front();
+            q.pop();
+            for(auto& V : adj[U]){
+                if(color[V] == color[U]){
+                    return false;
+                }
+                else if(color[V] == -1){
+                    color[V] = 1 - color[U];
+                    q.push(V);
+                }
+            }
         }
-
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
+
         unordered_map<int,vector<int>> adj;
-        for(int i=0;i<graph.size();i++){
-            for(auto& v : graph[i]){
-                adj[i].push_back(v);
+
+        for(int u=0;u<V;u++){
+            for(auto& v : graph[u]){
+                adj[u].push_back(v);
             }
         }
 
         vector<int> color(V,-1);
 
-        for(int i=0;i<V;i++){
-            if(color[i] == -1 && !dfs(i,adj,color,1))return false;
+        for(int u=0;u<V;u++){
+            if(color[u]==-1 && !bfs(u,adj,color,1))return false;
         }
 
         return true;
