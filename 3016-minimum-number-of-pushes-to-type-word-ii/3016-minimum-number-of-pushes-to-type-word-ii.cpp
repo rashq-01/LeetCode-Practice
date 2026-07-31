efa96388d8c3594f281a4b8015ba4af82134afb8
Rajesh Pandit <rashq122@gmail.com>
2026-07-31 09:28:03 +0530

    Time: 3 ms (92.87%), Space: 25.4 MB (72.47%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> mp(26,0);
-
+        vector<int> arr(26,0);
         for(auto& ch : word){
-            mp[ch - 'a']++;
+            arr[ch - 'a']++;
         }
-
-        sort(mp.begin(),mp.end(),greater<>());
-        int count = 0;
+        sort(arr.begin(),arr.end(),greater<>());
+        int ans = 0;
         for(int i=0;i<8;i++){
-            count+=mp[i];
+            ans +=arr[i];
         }
         for(int i=8;i<16;i++){
-            count+=(mp[i]*2);
+            ans += (arr[i]*2);
         }
         for(int i=16;i<24;i++){
-            count+=(mp[i]*3);
+            ans += (arr[i]*3);
         }
-        for(int i=24;i<26;i++){
-            count += (mp[i]*4);
-        }
+        ans += (arr[24] *4);
+        ans += (arr[25] * 4);
 
-        return count;
+
+        return ans;
     }
 };
