8ef0bf5ecb7b10972163b0082af35884e50302cf
Rajesh Pandit <rashq122@gmail.com>
2026-08-24 23:00:07 +0530

    Time: 110 ms (49.2%), Space: 95.3 MB (43.78%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int n;
-    int solve(int i,vector<int>& preSum,vector<int>& dp){
-        if(i==n-1)return preSum[i];
-
-        if(dp[i] != INT_MIN)return dp[i];
-        
-        int take = preSum[i] - solve(i+1,preSum,dp);
-        int skip = solve(i+1,preSum,dp);
-
-        dp[i] = max(take,skip);
-        return dp[i];
-    }
     int stoneGameVIII(vector<int>& stones) {
-        n = stones.size();
+        int n = stones.size();
         vector<int> preSum(n,0);
         preSum[0] = stones[0];
         for(int i=1;i<stones.size();i++){
             preSum[i] += (preSum[i-1]+stones[i]);
         }
-        vector<int> dp(n,INT_MIN);
 
-        return solve(1,preSum,dp);
+        vector<int> t(n,0);
+        t[n-1] = preSum[n-1];
+
+        for(int i=n-2;i>=1;i--){
+            int take = preSum[i] - t[i+1];
+            int skip = t[i+1];
+
+            t[i] = max(take,skip);
+        }
+
+        return t[1];
 
     }
 };
