978ca063774932846ac9f9e1d95c64426a84d4ac
Rajesh Pandit <rashq122@gmail.com>
2026-08-28 20:25:26 +0530

    Time: 10 ms (8.12%), Space: 8.9 MB (75.24%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1)return false;
+        if(n==1)return true;
+        if(n%3 != 0)return false;
+        while(n!=1){
+            if(n%3 != 0)return false;
+            n/=3;
+        }
+
+        return true;
+
+        
+    }
+};
