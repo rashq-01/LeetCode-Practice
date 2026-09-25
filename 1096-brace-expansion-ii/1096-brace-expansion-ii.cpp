a631071b10af55868ee972729ee531994a327a09
Rajesh Pandit <rashq122@gmail.com>
2026-09-25 09:56:48 -0700

    Time: 31 ms (5.8%), Space: 14.1 MB (89.29%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    set<string> ans;
+    void dfs(string s){
+        int r = s.find('}');
+
+        if(r == string::npos){
+            ans.insert(s);
+            return;
+        }
+
+        int l = s.rfind('{' , r);
+
+        string left = s.substr(0 , l);
+        string right = s.substr(r+1);
+
+        string inside = s.substr(l+1 , r-l-1);
+
+        string part;
+        stringstream ss(inside);
+        while(getline(ss , part , ',')){
+            dfs(left + part + right);
+        }
+
+    }
+    vector<string> braceExpansionII(string expression) {
+        dfs(expression);
+        return vector<string>(ans.begin() , ans.end());
+    }
+};
